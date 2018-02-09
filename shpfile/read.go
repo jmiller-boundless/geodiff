9c9490fdaf345ee0aae52f0e15dc0d34c18f28bf
jmiller-boundless <jmiller@boundlessgeo.com>
2018-02-09 15:02:23 -0500

    added equalsExact for polygons

--- shpfile/read.go
+++ shpfile/read.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"fmt"
 	"reflect"
+	"math"
 )
 
 func Open(path string){
@@ -31,4 +32,35 @@ func Open(path string){
 		fmt.Println()
 	}
 
+}
+
+type PolygonWrapper struct {
+	Polygon shp.Polygon
+}
+
+func (p PolygonWrapper) EqualsExact(other PolygonWrapper,tolerance float64) bool {
+	polygon := p.Polygon
+	otherpolygon := other.Polygon
+	if polygon.NumParts!=otherpolygon.NumParts{
+		return false
+	}
+	if polygon.NumPoints!=otherpolygon.NumPoints{
+		return false
+	}
+	for k,v :=range polygon.Points{
+		if !equalPoints(v,otherpolygon.Points[k],tolerance){
+			return false
+		}
+	}
+	return true
+}
+
+func equalPoints(point1 shp.Point, point2 shp.Point, tolerance float64) bool{
+	return pointDistance(point1,point2) <= tolerance
+}
+
+func pointDistance(point1 shp.Point, point2 shp.Point)float64{
+	dx := point1.X - point2.X
+	dy := point1.Y - point2.Y
+	return math.Sqrt(dx*dx+dy*dy)
 }
